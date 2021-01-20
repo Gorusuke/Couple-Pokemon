ab191f4466683e0c7511e2286fa829a1ab62babd
Jose Andres <Gorusuke123@gmail.com>
2021-01-20 01:07:24 -0500

    intentando implementar un arreglo para leer datos

---
+++ src/components/ListaRankings.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const ListaRankings = ({arreglo}) => {
+
+    // const {nombre, intentos} = arreglo;
+    console.info(arreglo)
+
+    // console.info(nombre)
+    // console.info(intentos)
+
+    return (
+        <div>
+            {/* <li>{nombre}{intentos}</li> */}
+        </div>
+    );
+}
+
+export default ListaRankings;
